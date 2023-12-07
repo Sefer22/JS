5215760d9e9fb1400ed81d8e3284e2299515c367
Sefer22 <120428751+Sefer22@users.noreply.github.com>
2023-12-08 02:16:02 +0400

    Add files via upload

--- JS-Examples/script.js
+++ JS-Examples/script.js
@@ -1,19 +1,30 @@
 
 
-function writeName() {
-    console.log("Safar");
+function write(name,surname) {
+    console.log(name + " " + surname);
 }
 
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
-writeName();
+write("Safar","Jafarli");
+write("Tunzala","Jafarova");
+
+function cube(number) {
+    console.log(number*number*number);
+}
+
+cube(3);
+cube(4);19
+cube(5);
+
+
+
+let age=Number(prompt("Enter your age"));
+
+check(age);
+
+function check(age) {
+    if(age > 18) {
+        console.log("You can buy Driver License");
+    }else {
+        console.log("You can't buy Driver License");
+    }
+}
